a44ad4279796bd686a9d970e0393ee7e2bb1a501
Michael Morgen <mmorgen@posteo.com>
2025-01-06 14:08:59 +0100

    Backup implementation part 1 (#815)
    * new component labels on most components
    * wide range of new RBAC permission for the backup ctrl
    * velero stack reconciliation
    * adjusted backup user-facing roles
    * added backup ctrl configuration in the hmc chart
    * removed schema defaults in favor of mutation

--- internal/controller/clusterdeployment_controller.go
+++ internal/controller/clusterdeployment_controller.go
@@ -52,6 +52,7 @@ import (
 	"github.com/K0rdent/kcm/internal/helm"
 	"github.com/K0rdent/kcm/internal/sveltos"
 	"github.com/K0rdent/kcm/internal/telemetry"
+	"github.com/K0rdent/kcm/internal/utils"
 	"github.com/K0rdent/kcm/internal/utils/status"
 )
 
@@ -144,6 +145,11 @@ func (r *ClusterDeploymentReconciler) reconcileUpdate(ctx context.Context, mc *h
 			return ctrl.Result{}, fmt.Errorf("failed to update clusterDeployment %s/%s: %w", mc.Namespace, mc.Name, err)
 		}
 		return ctrl.Result{}, nil
+	}
+
+	if err := utils.AddHMCComponentLabel(ctx, r.Client, mc); err != nil {
+		l.Error(err, "adding component label")
+		return ctrl.Result{}, err
 	}
 
 	if len(mc.Status.Conditions) == 0 {
