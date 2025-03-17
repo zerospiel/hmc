e801cc7269081130ba442b5f76971624b333306e
Bharath Nallapeta <bnallapeta@mirantis.com>
2025-03-17 17:31:21 +0530

    add leaderElectionNamespace flag in main.go - helps with debugging  (#1201)
    * add leaderElectionNamespace flag in main.go
    
    * run make generate-all
    
    Signed-off-by: Bharath Nallapeta <bnallapeta@mirantis.com>
    
    ---------
    
    Signed-off-by: Bharath Nallapeta <bnallapeta@mirantis.com>

--- cmd/main.go
+++ cmd/main.go
@@ -95,10 +95,12 @@ func main() {
 		webhookPort                int
 		webhookCertDir             string
 		pprofBindAddress           string
+		leaderElectionNamespace    string
 	)
 
 	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
+	flag.StringVar(&leaderElectionNamespace, "leader-election-namespace", "", "The namespace to use for leader election.")
 	flag.BoolVar(&secureMetrics, "metrics-secure", false,
 		"If set the metrics endpoint is served securely")
 	flag.BoolVar(&enableHTTP2, "enable-http2", false,
@@ -178,9 +180,10 @@ func main() {
 			SecureServing: secureMetrics,
 			TLSOpts:       tlsOpts,
 		},
-		HealthProbeBindAddress: probeAddr,
-		LeaderElection:         true,
-		LeaderElectionID:       "31c555b4.k0rdent.mirantis.com",
+		HealthProbeBindAddress:  probeAddr,
+		LeaderElection:          true,
+		LeaderElectionID:        "31c555b4.k0rdent.mirantis.com",
+		LeaderElectionNamespace: leaderElectionNamespace,
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
 		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
