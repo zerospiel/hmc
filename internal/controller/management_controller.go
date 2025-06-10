d937437e9f11ee8003ce48a20dd338b70b3a9eb2
Ekaterina Kazakova <41469478+eromanova@users.noreply.github.com>
2025-06-10 12:49:23 +0400

    fix: CAPI operator registry cert mount volume mismatch (#1621)
    Fixes #1620

--- internal/controller/management_controller.go
+++ internal/controller/management_controller.go
@@ -886,8 +886,9 @@ func processCAPIOperatorCertVolumeMounts(capiOperatorValues map[string]any, regi
 			"secretName":  "capi-operator-webhook-service-cert",
 		},
 	}
+	volumeName := "registry-cert"
 	registryCertVolume := map[string]any{
-		"name": "registry-cert",
+		"name": volumeName,
 		"secret": map[string]any{
 			"defaultMode": 420,
 			"secretName":  registryCertSecret,
@@ -918,7 +919,7 @@ func processCAPIOperatorCertVolumeMounts(capiOperatorValues map[string]any, regi
 	}
 	registryCertMount := map[string]any{
 		"mountPath": "/etc/ssl/certs/registry-ca.pem",
-		"name":      registryCertSecret,
+		"name":      volumeName,
 		"subPath":   "registry-ca.pem",
 	}
 	managerMounts := []any{webhookCertMount, registryCertMount}
